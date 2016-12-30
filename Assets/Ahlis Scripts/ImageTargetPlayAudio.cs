2be9085833b9c6fe313b1385e4770bcc2087db88
Ahlisen <ahlisen93@gmail.com>
2016-12-31 00:06:37 +0100

    Basic dandelion, basic track, knows when enter, can use audio

---
+++ Assets/Ahlis Scripts/ImageTargetPlayAudio.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAudio : MonoBehaviour,
+ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	private AudioSource audio;
+
+
+	public Transform cube;
+
+	void Start()
+	{
+		audio = GetComponent<AudioSource> ();
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	void Update() {
+		float vol = MicInput.MicLoudness;
+		if (vol > 0.01f) {
+			cube.transform.localEulerAngles += Vector3.up * vol * 3f;
+			Debug.Log(vol);
+		}
+
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			// Play audio when target is found
+			audio.Play();
+			StartCoroutine ("ScaleUp");
+		}
+		else
+		{
+			// Stop audio when target is lost
+			audio.Stop();
+		}
+	}  
+
+	IEnumerator ScaleUp () {
+		float percent = 0;
+		Vector3 normScale = cube.transform.localScale;
+
+		while (percent < 1) {
+
+			cube.transform.localScale = new Vector3(normScale.x, percent * normScale.y, normScale.z);
+
+			percent += 0.01f;
+			yield return null;
+		}
+
+		cube.transform.localScale = normScale;
+	}
+
+}
---
+++ Assets/WindReciever.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindReciever : MonoBehaviour {
+
+	Rigidbody rig;
+	Transform viewer;
+
+	// Use this for initialization
+	void Start () {
+		rig = GetComponent<Rigidbody> ();
+		viewer = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		rig.AddExplosionForce (100f, viewer.position, 10f);
+		//rig.AddForceAtPosition( Vector3.left * 30, viewer.position);
+	}
+}
