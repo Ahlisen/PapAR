d03350fb5362c5f6d587dbc703f0ac6269b833c5
Ahlisen <ahlisen93@gmail.com>
2017-01-06 19:31:16 +0100

    Half

---
+++ Assets/Ahlis Scripts/ImageVideo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageVideo : MonoBehaviour,
+ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+	public GameObject vid;
+	private Renderer rend;
+
+	void Start()
+	{
+		rend = vid.transform.GetComponent<Renderer> ();
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+
+		Color newColor = new Color(1, 1f, 1, 0);
+		rend.material.color = newColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			// Play audio when target is found
+			StartCoroutine(FadeTo(1f,1f));
+		}
+		else
+		{
+			// Stop audio when target is lost
+			//StartCoroutine(FadeTo(1f,1f));
+			Color newColor = new Color(1, 1f, 1, 0);
+			rend.material.color = newColor;
+		}
+	}
+
+
+	IEnumerator FadeTo(float aValue, float aTime)
+	{
+		float alpha = rend.material.color.a;
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+		{
+			Debug.Log (t);
+			Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha,aValue,t));
+			rend.material.color = newColor;
+			yield return null;
+		}
+	}
+
+}
---
+++ Assets/Ahlis Scripts/TitleVoice.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TitleVoice : MonoBehaviour,
+ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	private AudioSource audio;
+
+	public GameObject playButton;
+	//private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		//rend = playButton.GetComponents<Renderer> ();
+		//playButton.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckTouch ();
+	}
+
+	private void CheckTouch() {
+
+		#if UNITY_EDITOR
+		if (Input.GetMouseButtonDown(0)) {
+			// Construct a ray from the current mouse coordinates
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				checkTouchHit(hit);
+			}              
+		}
+		#endif
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			// Play audio when target is found
+			//audio.Play();
+			//playButton.SetActive(true);
+			//StartCoroutine ("ScaleUp");
+		}
+		else
+		{
+			//playButton.SetActive(false);
+			// Stop audio when target is lost
+			//audio.Stop();
+		}
+	}
+
+	private void checkTouchHit(RaycastHit hit) {
+		if(hit.transform.gameObject.tag == "Play") {
+			if (audio.isPlaying) {             
+				audio.Stop();
+			}
+			else {
+				audio.Play();
+			}
+		}
+	}
+}
