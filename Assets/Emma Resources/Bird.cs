7b74ec714ab3605a42cdd155af5d7a85f482deb9
Emma Klint <emma@emmaklint.com>
2017-01-06 12:07:07 +0100

    Added files for bird animation

---
+++ Assets/Emma Resources/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	private int speed = 1;
+	public float movementSpeed = 100;
+
+	private bool flying = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.LookAt (Camera.main.transform.position * -1);
+		transform.rotation = Quaternion.Euler(0,0, Mathf.Sin(Time.realtimeSinceStartup * speed) * 10);
+
+		// Change to run Flying if camera is close enough
+		if (Input.GetKeyDown ("space")) {
+			flying = true;
+		}
+
+		if (flying) {
+			Flying ();
+		}
+			
+	}
+
+	void Flying () {
+		speed = 5;
+		transform.Translate(new Vector3(1,0,0) * movementSpeed * Time.deltaTime);
+
+	}
+}
